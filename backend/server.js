6ea66a5485cf8cf45e01919adfbad29de3db477a
ammupriya1681 <ammupriya1681@gmail.com>
2025-09-02 00:00:06 +0530

    Initial backend commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import mongoose from "mongoose";
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 dotenv.config();
 
