12d0def3e05f45059617bc0da78f41c0ab88680d
ammupriya1681-alt <ammupriya1681@gmail.com>
2025-09-05 05:30:01 +0530

    Update AuthService.js

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -1,14 +1,13 @@
 import axios from "axios";
 
-// Backend API URL (deploy panna Render backend URL use pannu)
 const API_URL = "https://ott-backend-c7cv.onrender.com/api/auth";
 
 export const login = async (email, password) => {
   try {
     const res = await axios.post(`${API_URL}/login`, { email, password });
     return res.data;
-  } catch (err) {
-    throw err.response?.data || { message: "Login error" };
+  } catch (error) {
+    throw error.response?.data || { message: "Login failed" };
   }
 };
 
@@ -16,7 +15,7 @@ export const register = async (email, password) => {
   try {
     const res = await axios.post(`${API_URL}/register`, { email, password });
     return res.data;
-  } catch (err) {
-    throw err.response?.data || { message: "Register error" };
+  } catch (error) {
+    throw error.response?.data || { message: "Register failed" };
   }
 };
